532deb81144a2be70555684cc3dd580e0bb06913
Jan Wieck <jan@wi3ck.info>
2016-02-03 10:59:14 -0500

    Add convenience script runDatabaseBuild.sh that combines all commands necessary to create tables/objects, load data, build indexes and extras according to the properties file.
    Add convenience script runDatabaseDestroy.sh that removes
    all objects from the database. At this point it is more or
    less a wrapper around a command "runSQL.sh PROPS tableDrops".
    But I like symmetric commands.

---
+++ run/runDatabaseBuild.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ $# -ne 1 ] ; then
+    echo "usage: $(basename $0) PROPS" >&2
+    exit 2
+fi
+
+PROPS="$1"
+if [ ! -f "${PROPS}" ] ; then
+    echo "${PROPS}: no such file or directory" >&2
+    exit 1
+fi
+DB="$(grep '^db=' $PROPS | sed -e 's/^db=//')"
+
+BEFORE_LOAD="tableCreates"
+AFTER_LOAD="indexCreates extraHistID buildFinish"
+
+for step in ${BEFORE_LOAD} ; do
+    ./runSQL.sh "${PROPS}" $step
+done
+
+./runLoader.sh "${PROPS}"
+
+for step in ${AFTER_LOAD} ; do
+    ./runSQL.sh "${PROPS}" $step
+done

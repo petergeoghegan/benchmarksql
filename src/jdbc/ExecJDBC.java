2337a06ae102157032cb80920afe86e7e2d301e4
Mitchell Lussier <mitchell.lussier@openscg.com>
2016-08-02 14:54:06 -0400

    Fixed formatting errors from previous commit.

--- src/jdbc/ExecJDBC.java
+++ src/jdbc/ExecJDBC.java
@@ -14,6 +14,7 @@ import java.util.*;
 
 public class ExecJDBC {
 
+
   public static void main(String[] args) {
 
     Connection conn = null;
@@ -50,7 +51,9 @@ public class ExecJDBC {
 
       // loop thru input file and concatenate SQL statement fragments
       while((rLine = in.readLine()) != null) {
+
          String line = rLine.trim();
+
          if (line.length() != 0) {
            if (line.startsWith("--")) {
               System.out.println(rLine);  // print comment line

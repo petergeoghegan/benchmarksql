08e2f4dd29988f9e196f118da9c668240f9b807c
Mitchell Lussier <mitchell.lussier@openscg.com>
2016-08-08 14:17:37 -0400

    Changes made to optimize PAYMENT transaction in oracle.

--- run/sql.oracle/storedProcedureCreates.sql
+++ run/sql.oracle/storedProcedureCreates.sql
@@ -492,93 +492,70 @@ CREATE OR REPLACE PROCEDURE oracle_proc_payment(
 BEGIN
     out_h_date := CURRENT_TIMESTAMP;
 
-    --Update the DISTRICT
+    --Update and Select the DISTRICT
     UPDATE bmsql_district
 	SET d_ytd = d_ytd + in_h_amount
-	WHERE d_w_id = in_w_id AND d_id = in_d_id;
-
-    --Select the DISTRICT
-    SELECT  d_name, d_street_1, d_street_2,
-            d_city, d_state, d_zip
-    INTO out_d_name, out_d_street_1, out_d_street_2,
-         out_d_city, out_d_state, out_d_zip
-	FROM bmsql_district
 	WHERE d_w_id = in_w_id AND d_id = in_d_id
-	FOR UPDATE;
+    RETURNING d_name, d_street_1, d_street_2,
+	      d_city, d_state, d_zip
+	INTO out_d_name, out_d_street_1, out_d_street_2,
+	     out_d_city, out_d_state, out_d_zip;
 
     --Update the WAREHOUSE
     UPDATE bmsql_warehouse
         SET w_ytd = w_ytd + in_h_amount
-	WHERE w_id = in_w_id;
-
-    --Select the WAREHOUSE
-    SELECT  w_name, w_street_1, w_street_2, w_city,
-	    w_state, w_zip
-    INTO out_w_name, out_w_street_1, out_w_street_2,
-         out_w_city, out_w_state, out_w_zip
-	FROM bmsql_warehouse
 	WHERE w_id = in_w_id
-	FOR UPDATE;
+    RETURNING w_name, w_street_1, w_street_2, w_city,
+	      w_state, w_zip
+	INTO out_w_name, out_w_street_1, out_w_street_2,
+	     out_w_city, out_w_state, out_w_zip;
 
     -- If C_Last is given instead of C_ID (60%), determine the C_ID.
     IF in_c_last IS NOT NULL THEN
 	in_c_id := oracle_cid_from_clast(in_c_w_id, in_c_d_id, in_c_last);
     END IF;
 
-    -- Select the CUSTOMER
-    SELECT  c_first, c_middle, c_last, c_street_1,
-	    c_street_2, c_city, c_state, c_zip,
-	    c_phone, c_since, c_credit, c_credit_lim,
-	    c_discount, c_balance
-    INTO out_c_first, out_c_middle, in_c_last,
-	 out_c_street_1, out_c_street_2, out_c_city,
-         out_c_state, out_c_zip, out_c_phone, out_c_since,
-	 out_c_credit, out_c_credit_lim, out_c_discount,
-	 out_c_balance
-	FROM bmsql_customer
+    -- Update and Select the customer
+    UPDATE bmsql_customer
+	SET c_balance = c_balance - in_h_amount,
+	    c_ytd_payment = c_ytd_payment + in_h_amount,
+	    c_payment_cnt = c_payment_cnt + 1
 	WHERE c_w_id = in_c_w_id AND c_d_id = in_c_d_id AND c_id = in_c_id
-	FOR UPDATE;
+    RETURNING c_first, c_middle, c_last, c_street_1,
+	      c_street_2, c_city, c_state, c_zip,
+	      c_phone, c_since, c_credit, c_credit_lim,
+	      c_discount, c_balance
+	INTO out_c_first, out_c_middle, in_c_last,
+	     out_c_street_1, out_c_street_2, out_c_city,
+	     out_c_state, out_c_zip, out_c_phone, out_c_since,
+	     out_c_credit, out_c_credit_lim, out_c_discount,
+	     out_c_balance;
 
-    --Update the CUSTOMER
     out_c_balance := out_c_balance - in_h_amount;
-    IF out_c_credit = 'GC' THEN
+    out_c_data := ' ';
+
+    --Customer with bad credit, need to do the C_DATA work.
+    IF out_c_credit = 'BC' THEN
 	UPDATE bmsql_customer
-            SET c_balance = c_balance - in_h_amount,
-                c_ytd_payment = c_ytd_payment + in_h_amount,
-                c_payment_cnt = c_payment_cnt +1
-	    WHERE c_w_id = in_c_w_id AND c_d_id = in_c_d_id AND c_id = in_c_id;
-	out_c_data := ' ';
-    ELSE
-        --Customer with bad credit, need to do the C_DATA work.
-        SELECT c_data
-        INTO out_c_data
-	    FROM bmsql_customer
-	    WHERE c_w_id = in_c_w_id AND c_d_id = in_c_d_id
-	      AND c_id = in_c_id;
-        out_c_data := SUBSTR('C_ID='	  || TO_CHAR(in_c_id)   ||
-			     ' C_D_ID='	  || TO_CHAR(in_c_d_id) ||
-                             ' C_W_ID='	  || TO_CHAR(in_c_w_id) ||
-                             ' D_ID='	  || TO_CHAR(in_d_id)   ||
-                             ' W_ID='	  || TO_CHAR(in_w_id)   ||
+	    SET c_data = SUBSTR('C_ID='      || TO_CHAR(in_c_id)   ||
+                             ' C_D_ID='   || TO_CHAR(in_c_d_id) ||
+                             ' C_W_ID='   || TO_CHAR(in_c_w_id) ||
+                             ' D_ID='     || TO_CHAR(in_d_id)   ||
+                             ' W_ID='     || TO_CHAR(in_w_id)   ||
                              ' H_AMOUNT=' || ROUND(in_h_amount,2),
-			       1, 500);
+                               1, 500)
+	    WHERE c_w_id = in_c_w_id AND c_d_id = in_c_d_id AND c_id = in_c_id
+	RETURNING c_data
+	    INTO out_c_data;
+    END IF;
 
-	UPDATE bmsql_customer
-	    SET c_balance = c_balance - in_h_amount,
-		c_ytd_payment = c_ytd_payment + in_h_amount,
-		c_payment_cnt = c_payment_cnt + 1,
-		c_data = out_c_data
-	    WHERE c_w_id = in_c_w_id AND c_d_id = in_c_d_id
-	      AND c_id = in_c_id;
-        END IF;
-
-        --Insert the HISTORY row
-        INSERT INTO bmsql_history (
-                h_c_id, h_c_d_id, h_c_w_id, h_d_id, h_w_id,
-                h_date, h_amount, h_data)
-        VALUES (
-                in_c_id, in_c_d_id, in_c_w_id, in_d_id, in_w_id,
-                out_h_date, in_h_amount, out_w_name||'    '||out_d_name);
+    --Insert the HISTORY row
+    INSERT INTO bmsql_history (
+            h_c_id, h_c_d_id, h_c_w_id, h_d_id, h_w_id,
+            h_date, h_amount, h_data)
+    VALUES (
+            in_c_id, in_c_d_id, in_c_w_id, in_d_id, in_w_id,
+            out_h_date, in_h_amount, out_w_name||'    '||out_d_name);
 END;
 -- }
 
